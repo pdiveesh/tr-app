be21b1417d7eb39903be6213232bad24ef14db9a
UzumakiAnirudh <naruffy116@gmail.com>
2024-05-31 11:02:05 +0530

    Update Subjectcontroller.java

--- tr-app/src/main/java/com/example/tr/app/controller/Subjectcontroller.java
+++ tr-app/src/main/java/com/example/tr/app/controller/Subjectcontroller.java
@@ -4,27 +4,29 @@ import com.example.tr.app.model.Subject;
 import com.example.tr.app.service.SubjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Collection;
+import java.util.List;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:8080", "http://localhost:5173"})
-@RequestMapping("/api/Subjectdetails")
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/subjects")
 public class Subjectcontroller {
-    private final SubjectService service;
 
-    @Autowired
-    public Subjectcontroller(SubjectService service) {
-        this.service = service;
-    }
+    private final SubjectService subjectService;
 
-    @GetMapping("/get")
-    public Collection<Subject> getDetails() {
-        return service.getDetails();
+    public Subjectcontroller(SubjectService subjectService) {
+        this.subjectService = subjectService;
     }
 
     @PostMapping("/add")
-    public void postDetails(@RequestBody Subject subject) {
-        service.postDetails(subject);
+    public String createSubject(@RequestParam String subjectName, @RequestParam("subjectIcon") MultipartFile subjectIcon) {
+        return subjectService.createSubject(subjectName, subjectIcon);
+    }
+
+    @GetMapping("/getsubjects")
+    public List<Subject> getAllSubjects() {
+        return subjectService.getAllSubjects();
     }
 }
